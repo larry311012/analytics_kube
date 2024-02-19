d664629f42fd2ced7badc5556b3f7e22f288336a
larry311012 <larry311012@gmai.com>
2024-02-18 23:20:24 -0800

    add python files

---
+++ dags/air_dag.py
@@ -0,0 +1,33 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator  
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.python_operator import PythonOperato
+from datetime import datetime, timedelta
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2024, 2, 18),
+    'email': ['your_email@example.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
+
+dag = DAG(
+    'air_DAG',
+    default_args=default_args,
+    description='A simple DAG to run schedule.py with environment variables',
+    schedule_interval=timedelta(minutes=3),
+    catchup=False,  # Add this to prevent backfilling if your start_date is in the past
+)
+
+t1 = BashOperator(
+    task_id='run_schedule_py',
+    bash_command='python3 $AIRFLOW_HOME/dags/schedule.py',
+    env={'AIRLABS_API_KEY': '{{ var.value.AIRLABS_API_KEY }}'},  # Example of using Airflow Variables
+    dag=dag,
+)
+
+t1
---
+++ dags/schedule.py
@@ -0,0 +1,54 @@
+import pandas as pd
+import requests
+import os
+import sys
+import boto3
+from io import StringIO  # For string IO
+
+def extract(api_key):
+    fields = "&_fields=flight_iata,dep_iata,dep_time_utc,arr_iata,arr_time_utc,status,duration,delayed,dep_delayed,arr_delayed"
+    schedules_api = 'https://airlabs.co/api/v9/schedules?airline_iata=FR'
+    print("Extracting...")
+    try:
+        response = requests.get(f"{schedules_api}{fields}", params={'api_key': api_key})
+        response.raise_for_status()  # This will raise an exception for HTTP error codes
+        schedule_data = pd.json_normalize(response.json(), record_path=['response'])
+        return schedule_data
+    except requests.exceptions.RequestException as e:
+        print(f"Request failed: {e}", file=sys.stderr)
+        sys.exit(1)
+
+def save_to_s3(data, bucket_name, object_name):
+    """
+    Save the DataFrame to a CSV file and upload it to S3.
+
+    :param data: DataFrame to save.
+    :param bucket_name: S3 bucket name.
+    :param object_name: S3 object name. Include the file name to save as in the bucket.
+    """
+    # Convert DataFrame to CSV string
+    csv_buffer = StringIO()
+    data.to_csv(csv_buffer, index=False)
+
+    # Create S3 client
+    s3_client = boto3.client('s3')
+
+    # Upload CSV to S3
+    s3_client.put_object(Bucket=bucket_name, Key=object_name, Body=csv_buffer.getvalue())
+    print(f"Data uploaded to s3://{bucket_name}/{object_name}")
+
+def main():
+    api_key = os.getenv("AIRLABS_API_KEY")
+    if not api_key:
+        print("API key not found. Set the AIRLABS_API_KEY environment variable.")
+        sys.exit(1)
+
+    # Define your S3 bucket and object name
+    bucket_name = 'analytics-kube'
+    object_name = 'schedule.csv'
+
+    data = extract(api_key)
+    save_to_s3(data, bucket_name, object_name)
+
+if __name__ == "__main__":
+    main()
