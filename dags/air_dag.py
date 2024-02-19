2ec3a7317c63ffee0474ba5e27eeeb150a828e98
larry311012 <larry311012@gmai.com>
2024-02-19 11:46:59 -0800

    update datetime

--- dags/air_dag.py
+++ dags/air_dag.py
@@ -5,12 +5,12 @@ from datetime import datetime, timedelta
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
-    'start_date': datetime(2024, 2, 18),
+    'start_date': datetime(2024, 2, 19),
     'email': ['your_email@example.com'],
     'email_on_failure': False,
     'email_on_retry': False,
     'retries': 1,
-    'retry_delay': timedelta(minutes=5),
+    'retry_delay': timedelta(minutes=1),
 }
 
 dag = DAG(
