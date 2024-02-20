949fdb060342b6d19d3d3bbfc10a7f54cc952b80
larry311012 <larry311012@gmai.com>
2024-02-19 17:33:02 -0800

    debug example

--- dags/example.py
+++ dags/example.py
@@ -2,10 +2,21 @@ from airflow import DAG
 from airflow.operators.python import PythonOperator
 from datetime import datetime
 
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2024, 2, 19),
+    'email': ['your_email@example.com'], 
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=1),
+}
+
 def my_callable(*args, **kwargs):
     print("Hello from PythonOperator")
 
-with DAG('my_dag', start_date=datetime(2021, 1, 1)) as dag:
+with DAG('my_dag', default_args=default_args, start_date=datetime(2021, 1, 1)) as dag:
     python_task = PythonOperator(
         task_id='my_python_task',
         python_callable=my_callable
